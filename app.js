60d0b69598e78b47aefc393eccdea34ac61e38c8
Abdul-fatai <staticdev20046@gmail.com>
2019-10-25 18:43:25 +0100

    UIController modified

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ const ItemCtrl = (function(){
 
     //Public methods
     return {
+        getData: function(){
+            return data.items;
+        },
         logData: function(){
             return data;
         }
@@ -32,10 +35,26 @@ const ItemCtrl = (function(){
 
 //UI Controller 
 const UICtrl = (function(){
-
+    let UIselectors = {
+        item: '#item-list'
+    }
     //Public methods
     return {
-
+        populateItemList: function (items) {
+            let html = '';
+            items.forEach(item => {
+                html += `
+                <li class="collection-item" id="item-${item.id}">
+                <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                <a href="#" class="secondary-content">
+                    <i class="edit-item fa fa-pencil"></i>
+                </a>
+                </li>
+                `;
+            });
+            // Insert Items to the DOM
+            document.querySelector(UIselectors.item).innerHTML = html;
+        }
     }
 })();
 
@@ -47,7 +66,11 @@ const App = (function(ItemCtrl, UICtrl){
     //Public methods
     return {
         init: function(){
-            console.log("Initializing App....");
+            //Fetch items from data structure 
+            const items = ItemCtrl.getData();
+
+            // Populate list with items
+            UICtrl.populateItemList(items);
         }
     }
 })(ItemCtrl, UICtrl);
